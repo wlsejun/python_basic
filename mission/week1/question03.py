985d3c6117639e31dab71e6c39f139bdc4265ff7
wlsejun <worldlight97@daum.net>
2023-07-13 21:21:56 +0900

    python error 종류

---
+++ mission/week1/question03.py
@@ -0,0 +1,39 @@
+# ---------------Syntax Error-------------------
+# Syntax Error_Example01
+x = 6
+if x > 5
+print("x > 5")
+
+# Syntax Error_Debugging01
+x = 6
+if x > 5:
+    print("x > 5")
+
+# Syntax Error_Example02
+name = input("what's your name?")
+print("hello, name)
+# Syntax Error_Debugging02
+name = input("what's your name?")
+print("hello", name)
+
+# ---------------Value Error-------------------
+# Value Error_Example01
+language = "english"
+lan_code = int(language)
+
+# Value Error_Debugging01
+language = "010324"
+lan_code = int(language)
+print(lan_code)
+
+# ---------------Type Error-------------------
+# Type Error_Example01
+myName = "Lisa"
+myAge = 27
+me = myName + myAge
+
+# Type Error_Debugging01
+myName = "Lisa"
+myAge = "27"
+me = myName + myAge
+print("I'm", me)
