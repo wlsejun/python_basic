e77a184ad1c68f29e6394b7f5b5cae52af2dd3c6
wlsejun <worldlight97@daum.net>
2023-07-18 09:59:05 +0900

    if, elif practice

--- 06팀_1주차_04.py
+++ 06팀_1주차_04.py
@@ -6,3 +6,26 @@ kor_age = int(input('한국 나이 : '))
 # 미국식 나이 = 한국식 나이 + birth
 birth = int(input('생일이 지났습니까? 맞으면 0, 틀리면 -1 : '))
 print(name,'의 미국 나이는 %d살 입니다.'%(kor_age + birth))
+
+
+
+# =====================아래는 추가로 피드백 받고 싶은 코드입니다====================
+birth_year=input('when did you born?')
+birth_month=input('when is your birth month?')
+birth_day=input('when is your birth date?')
+birth_y=int(birth_year)
+birth_m=int(birth_month)
+birth_d=int(birth_day)
+time_y=int(now.year)
+time_m=int(now.month)
+time_d=int(now.day)
+age=time_y-birth_y
+if birth_m > time_m :
+    age = age-1
+else :
+    if (birth_m == time_m) and (birth_d  >time_d) :
+        age = age -1
+    else :
+        age = age
+print(age)
+int(input('생일에 언제세요 : '))
---
+++ mission/week2/test.py
@@ -0,0 +1,10 @@
+x = 50
+if x < 80 :
+    print("x less than 10")
+elif x < 60 :
+    print("x less than 60")
+elif x < 70 :
+    print("x less than 70")
+else :
+    print("else")
+print("all done")
