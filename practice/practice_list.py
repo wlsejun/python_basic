407086def41eddab8fb7193c76563976ffe5c152
wlsejun <worldlight97@daum.net>
2023-07-24 12:20:21 +0900

    list 연습 코드

---
+++ practice/practice_list.py
@@ -0,0 +1,18 @@
+friends = ['yonghou', 2, 'hyejin']
+
+for i in range(len(friends)):
+    friend = friends[i]
+    print('hello', friend)
+print(friends)
+friends[1] = 'hi'
+print(friends)
+frined = 'Byun'
+x = frined.lower()
+print(x)
+
+
+print(range(4))
+print(range(len(friends)))
+
+for i in range(3):
+    print(i)
