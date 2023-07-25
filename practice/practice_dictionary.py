d47dcaeb1d7b35de0f7f07c813bb616ba018318d
wlsejun <worldlight97@daum.net>
2023-07-25 18:03:35 +0900

    미션 취합본 수정

--- practice/practice_dictionary.py
+++ practice/practice_dictionary.py
@@ -1,2 +1,20 @@
 score = {'english' : 99, 'math' : 100, 'chinese' : 95}
 print(score)
+
+pack = dict()
+pack['class'] = 5
+pack['language'] = 4
+pack['class'] += 1
+print(pack)
+'class' in pack
+
+counts = dict()
+names = ['hyejin', 'sungsoo', 'hyejin', 'sejun', 'yonghao', 'sungsoo', 'hyejin']
+for name in names :
+    if name not in counts :
+        counts[name] = 1
+    else :
+        counts[name] = counts[name] + 1
+print(counts)
+
+# get() 메서드
